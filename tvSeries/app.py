645069d75420eed0e031ea79f7f5a1348e933723
Dalia <dalia.dahleh@gmail.com>
2019-03-11 10:43:51 -0400

    ui fixes +  moved gitignore

--- tvSeries/app.py
+++ tvSeries/app.py
@@ -42,12 +42,6 @@ def html_lookup(page):
     except TemplateNotFound:
         abort(404)
 
-
-# @app.route("/")
-# def index():
-#     """Return the homepage."""
-#     return render_template("index.html")
-
 @app.route("/names")
 def names():
     """Return a list of sample names."""
